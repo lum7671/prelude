0276194edd510bc8ad21dea9f691cb213e098763
Nate Doohyun Jang <dh.jang@gmail.com>
2020-05-21 23:03:18 +0900

    설정 변경

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -1,21 +1,21 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
 ;; Emacs IRC client
-(require 'prelude-erc)
-;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+;; (require 'prelude-erc)
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-ivy) ;; A mighty modern alternative to ido
-;; (require 'prelude-helm) ;; Interface for narrowing and search
-;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
-;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
-;; (require 'prelude-evil)
+(require 'prelude-key-chord) ;; Binds useful features to key combinations
+(require 'prelude-evil)
 
 ;;; Programming languages support
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
-;; (require 'prelude-common-lisp)
-;; (require 'prelude-css)
+(require 'prelude-common-lisp)
+(require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
@@ -28,7 +28,7 @@
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-;; (require 'prelude-python)
+(require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-rust)
 ;; (require 'prelude-scala)
@@ -36,6 +36,6 @@
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-ts)
-;; (require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-;; (require 'prelude-yaml)
+(require 'prelude-yaml)
