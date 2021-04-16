ccb1fecae4508a0a58b6e81f24366daa1240d320
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2021-04-16 12:45:49 +0900

    정비

--- personal/preload/preload-elpa.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
--- personal/preload/preload-etc.el
+++
@@ -1,6 +0,0 @@
-;; * [GitHub - Atman50/emacs-config: A literate emacs configuration for C#, python, ivy, yasnippet, ...](https://github.com/Atman50/emacs-config#org5309cf1)
-
-(setq gc-cons-threshold 64000000)
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
-
-(setq auto-window-vscroll nil)
